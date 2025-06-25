786d499747aea9b6fd74923e39e9fffd238d5d51
somenewartistfromukraine <somenewartistfromukraine@gmail.com>
2025-06-25 16:43:26 +0300

    fixed errors

--- src/components/map/MapContainer.tsx
+++ src/components/map/MapContainer.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { MapContainer as LeafletMap, TileLayer, useMap } from 'react-leaflet';
-import { useEffect } from 'react';
+import React, { useMemo } from 'react';
+import { MapContainer as LeafletMap, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useMapStore } from '../../stores/mapStore';
 import { getMemoizedTileLayerUrl, getMemoizedTileLayerAttribution, getMemoizedTileLayerOptions } from '../../utils/memoize';
+import L from 'leaflet';
 
 interface MapContainerProps {
   height?: string;
@@ -15,19 +15,45 @@ const MapContainer: React.FC<MapContainerProps> = ({
   const { center, zoom, activeLayer } = useMapStore();
   const tileLayerOptions = getMemoizedTileLayerOptions(activeLayer);
 
+  // Ensure we have valid center coordinates
+  const mapCenter = useMemo(() => 
+    center && Array.isArray(center) && center.length === 2 && !isNaN(center[0]) && !isNaN(center[1])
+      ? [center[0], center[1]] as L.LatLngExpression
+      : [51.505, -0.09] as L.LatLngExpression, // Default to London if center is invalid
+    [center]
+  );
+
+  // Ensure we have a valid zoom level
+  const mapZoom = useMemo(() => 
+    typeof zoom === 'number' && !isNaN(zoom) ? zoom : 13,
+    [zoom]
+  );
+
+  // Get tile layer props
+  const tileLayerProps = useMemo(() => ({
+    url: getMemoizedTileLayerUrl(activeLayer),
+    attribution: getMemoizedTileLayerAttribution(activeLayer),
+    ...tileLayerOptions,
+    // Ensure subdomains is always an array
+    subdomains: tileLayerOptions.subdomains || ['a', 'b', 'c']
+  }), [activeLayer, tileLayerOptions]);
+
   return (
     <div style={{ height: height }}>
       <LeafletMap 
-        center={center} 
-        zoom={zoom} 
-        style={{ height: '100%' }}
-        zoomControl={false} // Disable default zoom controls
+        center={mapCenter}
+        zoom={mapZoom}
+        style={{ height: '100%', width: '100%' }}
+        zoomControl={false}
+        minZoom={2}
+        maxZoom={18}
+        maxBounds={[
+          [-90, -180],
+          [90, 180]
+        ]}
+        maxBoundsViscosity={1.0}
       >
-        <TileLayer
-          url={getMemoizedTileLayerUrl(activeLayer)}
-          attribution={getMemoizedTileLayerAttribution(activeLayer)}
-          {...tileLayerOptions}
-        />
+        <TileLayer {...tileLayerProps} />
       </LeafletMap>
     </div>
   );
